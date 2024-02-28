1195e89b70efb22dc62f420ecf54edc4eac0e183
Jeff Garzik <jgarzik@pobox.com>
2024-02-28 01:31:37 -0500

    add util: ln (half-implemented)

---
+++ link/src/ln.rs
@@ -0,0 +1,70 @@
+//
+// Copyright (c) 2024 Jeff Garzik
+//
+// This file is part of the posixutils-rs project covered under
+// the MIT License.  For the full license text, please see the LICENSE
+// file in the root directory of this project.
+// SPDX-License-Identifier: MIT
+//
+
+extern crate clap;
+extern crate plib;
+
+use clap::Parser;
+use gettextrs::{bind_textdomain_codeset, gettext, textdomain};
+use plib::PROJECT_NAME;
+use std::{fs, io};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about)]
+struct Args {
+    /// Force existing destination pathnames to be removed to allow the link.
+    #[arg(short, long)]
+    force: bool,
+
+    /// Create symbolic links instead of hard links.
+    #[arg(short, long)]
+    symlink: bool,
+
+    /// Source(s) and target of link(s).
+    files: Vec<String>,
+}
+
+fn do_link(args: &Args, file1: &str, file2: &str) -> io::Result<()> {
+    if args.symlink {
+        fs::soft_link(file1, file2)
+    } else {
+        fs::hard_link(file1, file2)
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // parse command line arguments
+    let args = Args::parse();
+
+    textdomain(PROJECT_NAME)?;
+    bind_textdomain_codeset(PROJECT_NAME, "UTF-8")?;
+
+    if args.files.len() < 2 {
+        eprintln!("{}", gettext("Must supply a source and target for linking"));
+        std::process::exit(1);
+    }
+
+    let sources = &args.files[0..args.files.len() - 1];
+    let target = &args.files[args.files.len() - 1];
+
+    let mut exit_code = 0;
+
+    if sources.len() == 1 {
+        let src = &sources[0];
+        match do_link(&args, src, target) {
+            Ok(()) => {}
+            Err(e) => {
+                exit_code = 1;
+                eprintln!("{} -> {}: {}", src, target, e);
+            }
+        }
+    }
+
+    std::process::exit(exit_code)
+}

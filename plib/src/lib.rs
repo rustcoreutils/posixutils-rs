435eadb98c5076c296b889ed7b595b6bde2f9885
Jeff Garzik <jgarzik@pobox.com>
2024-03-10 11:35:06 -0400

    Move generic testing code to plib

--- plib/src/lib.rs
+++ plib/src/lib.rs
@@ -7,6 +7,44 @@
 // SPDX-License-Identifier: MIT
 //
 
+use std::io::Write;
+use std::process::{Command, Stdio};
+
 pub const PROJECT_NAME: &'static str = "posixutils-rs";
 
 pub const BUFSZ: usize = 8 * 1024;
+
+pub struct TestPlan {
+    pub cmd: String,
+    pub args: Vec<String>,
+    pub stdin_data: String,
+    pub expected_out: String,
+}
+
+pub fn run_test(plan: TestPlan) {
+    let relpath = format!("target/release/{}", plan.cmd);
+    let test_bin_path = std::env::current_dir()
+        .unwrap()
+        .parent()
+        .unwrap() // Move up to the workspace root from the current package directory
+        .join(relpath); // Adjust the path to the binary
+
+    let mut command = Command::new(test_bin_path);
+    let mut child = command
+        .args(plan.args)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to spawn head");
+
+    let stdin = child.stdin.as_mut().expect("failed to get stdin");
+    stdin
+        .write_all(plan.stdin_data.as_bytes())
+        .expect("failed to write to stdin");
+
+    let output = child.wait_with_output().expect("failed to wait for child");
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    assert_eq!(stdout, plan.expected_out);
+    assert!(output.status.success());
+    assert_eq!(output.status.code(), Some(0));
+}

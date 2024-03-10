aa63835f0cfd5f2e7641c648d6c894bfdb1ed844
Jeff Garzik <jgarzik@pobox.com>
2024-03-10 00:07:36 -0500

    head: add basic tests

--- text/tests/integration.rs
+++ text/tests/integration.rs
@@ -10,6 +10,50 @@
 use std::io::Write;
 use std::process::{Command, Stdio};
 
+struct TestPlan {
+    cmd: String,
+    args: Vec<String>,
+    stdin_data: String,
+    expected_out: String,
+}
+
+fn run_test(plan: TestPlan) {
+    let relpath = format!("target/release/{}", plan.cmd);
+    let test_bin_path = std::env::current_dir()
+        .unwrap()
+        .parent()
+        .unwrap() // Move up to the workspace root from the current package directory
+        .join(relpath); // Adjust the path to the binary
+
+    let mut command = Command::new(test_bin_path);
+    let mut child = command
+        .args(plan.args)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to spawn head");
+
+    let stdin = child.stdin.as_mut().expect("failed to get stdin");
+    stdin
+        .write_all(plan.stdin_data.as_bytes())
+        .expect("failed to write to stdin");
+
+    let output = child.wait_with_output().expect("failed to wait for child");
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    assert_eq!(stdout, plan.expected_out);
+    assert!(output.status.success());
+    assert_eq!(output.status.code(), Some(0));
+}
+
+fn head_test(test_data: &str, expected_output: &str) {
+    run_test(TestPlan {
+        cmd: String::from("head"),
+        args: Vec::new(),
+        stdin_data: String::from(test_data),
+        expected_out: String::from(expected_output),
+    });
+}
+
 fn wc_test(args: &[&str], test_data: &str, expected_output: &str) {
     let test_bin_path = std::env::current_dir()
         .unwrap()
@@ -35,6 +79,19 @@ fn wc_test(args: &[&str], test_data: &str, expected_output: &str) {
     assert_eq!(stdout, expected_output);
     assert!(output.status.success());
     assert_eq!(output.status.code(), Some(0));
+}
+
+#[test]
+fn test_head_basic() {
+    head_test("a\nb\nc\nd\n", "a\nb\nc\nd\n");
+    head_test(
+        "1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n",
+        "1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n",
+    );
+    head_test(
+        "1\n2\n3\n4\n5\n6\n7\n8\n9\n0\na\n",
+        "1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n",
+    );
 }
 
 #[test]

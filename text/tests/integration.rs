067320e3812ec5580bf7defec948276b7080cbee
Jeff Garzik <jgarzik@pobox.com>
2024-03-10 00:23:48 -0500

    wc: simplify tests

--- text/tests/integration.rs
+++ text/tests/integration.rs
@@ -55,30 +55,14 @@ fn head_test(test_data: &str, expected_output: &str) {
 }
 
 fn wc_test(args: &[&str], test_data: &str, expected_output: &str) {
-    let test_bin_path = std::env::current_dir()
-        .unwrap()
-        .parent()
-        .unwrap() // Move up to the workspace root from the current package directory
-        .join("target/release/wc"); // Adjust the path to the binary
+    let str_args: Vec<String> = args.iter().map(|s| String::from(*s)).collect();
 
-    let mut command = Command::new(test_bin_path);
-    let mut child = command
-        .args(args)
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("failed to spawn wc");
-
-    let stdin = child.stdin.as_mut().expect("failed to get stdin");
-    stdin
-        .write_all(test_data.as_bytes())
-        .expect("failed to write to stdin");
-
-    let output = child.wait_with_output().expect("failed to wait for child");
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    assert_eq!(stdout, expected_output);
-    assert!(output.status.success());
-    assert_eq!(output.status.code(), Some(0));
+    run_test(TestPlan {
+        cmd: String::from("wc"),
+        args: str_args,
+        stdin_data: String::from(test_data),
+        expected_out: String::from(expected_output),
+    });
 }
 
 #[test]

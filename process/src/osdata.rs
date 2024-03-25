c9cf085eb21877e10a134988e1718f3448f4b1de
Jeff Garzik <jgarzik@pobox.com>
2024-03-24 21:28:15 -0400

    kill: add linux signals

--- process/src/osdata.rs
+++ process/src/osdata.rs
@@ -47,3 +47,41 @@ pub fn get_sigmap() -> HashMap<&'static str, u32> {
         ("USR2", 31),
     ])
 }
+
+#[cfg(target_os = "linux")]
+pub fn get_sigmap() -> HashMap<&'static str, u32> {
+    HashMap::from([
+        ("HUP", 1),
+        ("INT", 2),
+        ("QUIT", 3),
+        ("ILL", 4),
+        ("TRAP", 5),
+        ("ABRT", 6),
+        ("IOT", 6),
+        ("BUS", 7),
+        ("FPE", 8),
+        ("KILL", 9),
+        ("USR1", 10),
+        ("SEGV", 11),
+        ("USR2", 12),
+        ("PIPE", 13),
+        ("ALRM", 14),
+        ("TERM", 15),
+        ("STKFLT", 16),
+        ("CHLD", 17),
+        ("CONT", 18),
+        ("STOP", 19),
+        ("TSTP", 20),
+        ("TTIN", 21),
+        ("TTOU", 22),
+        ("URG", 23),
+        ("XCPU", 24),
+        ("XFSZ", 25),
+        ("VTALRM", 26),
+        ("PROF", 27),
+        ("WINCH", 28),
+        ("IO", 29),
+        ("PWR", 30),
+        ("SYS", 31),
+    ])
+}

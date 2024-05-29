3cd3569f982013c869077d8bd4fad15a919815b3
Luke Frisken <l.frisken@gmail.com>
2024-05-29 12:59:24 +1000

    m4: Added recurse test

--- m4/tests/integration_test.rs
+++ m4/tests/integration_test.rs
@@ -604,6 +604,29 @@ fn test_macro_errprint_no_evaluation_quoted() {
 }
 
 #[test]
+fn test_recurse() {
+    init();
+    let output = run_command("fixtures/integration_tests/recurse.m4");
+
+    let test: TestSnapshot = read_test("fixtures/integration_tests/recurse.out");
+    assert_eq!(
+        output.status,
+        std::process::ExitStatus::from_raw(test.status),
+        "status (\x1b[31mcurrent\x1b[0m|\x1b[32mexpected\x1b[0m)"
+    );
+    assert_eq!(
+        String::from_utf8(output.stdout).unwrap(),
+        test.stdout,
+        "stdout (\x1b[31mcurrent\x1b[0m|\x1b[32mexpected\x1b[0m)"
+    );
+    assert_eq!(
+        String::from_utf8(output.stderr).unwrap(),
+        test.stderr,
+        "stderr (\x1b[31mcurrent\x1b[0m|\x1b[32mexpected\x1b[0m)"
+    );
+}
+
+#[test]
 fn test_recursive_defines() {
     init();
     let output = run_command("fixtures/integration_tests/recursive_defines.m4");

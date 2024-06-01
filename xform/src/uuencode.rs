3d9f9035c6b549c2ebb37078dcef504f99e4ec89
rishadbaniya <rbdroid01@gmail.com>
2024-06-02 00:32:18 +0545

    [uuencode] refactor: add macos conditional compilation

--- xform/src/uuencode.rs
+++ xform/src/uuencode.rs
@@ -124,7 +124,18 @@ fn encode_file(args: &Args) -> io::Result<()> {
         .clone();
 
     if file_p == PathBuf::from("/dev/stdin") {
-        let mode = RW & (!unsafe { libc::umask(RW) } as u32);
+        let mode = {
+            #[cfg(target_os = "macos")]
+            {
+                RW & (!unsafe { libc::umask(RW as u16) } as u32)
+            }
+
+            #[cfg(not(target_os = "macos"))]
+            {
+                RW & (!unsafe { libc::umask(RW) })
+            }
+        };
+
         let perm = get_permission_values(Permissions::from_mode(mode));
         let header = format!("{header_init} {perm} {decode_path}\n");
 

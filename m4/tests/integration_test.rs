b5fb880a8b6fd5d575745468e666b124849620bf
Luke Frisken <l.frisken@gmail.com>
2024-06-06 20:43:58 +1000

    m4: Started trying to integrate the bsd test suite there are a few issues though

--- m4/tests/integration_test.rs
+++ m4/tests/integration_test.rs
@@ -64,6 +64,56 @@ fn run_command(input: &str) -> std::process::Output {
     }
 }
 
+#[ignore]
+#[test]
+fn test_bsd() {
+    init();
+    let output = run_command("fixtures/integration_tests/bsd.m4");
+
+    let test: TestSnapshot = read_test("fixtures/integration_tests/bsd.out");
+    assert_eq!(
+        output.status,
+        std::process::ExitStatus::from_raw(test.status),
+        "status (\x1b[31mcurrent\x1b[0m|\x1b[32mexpected\x1b[0m)"
+    );
+    assert_eq!(
+        String::from_utf8(output.stdout).unwrap(),
+        test.stdout,
+        "stdout (\x1b[31mcurrent\x1b[0m|\x1b[32mexpected\x1b[0m)"
+    );
+
+    assert_eq!(
+        String::from_utf8(output.stderr).unwrap(),
+        test.stderr,
+        "stderr (\x1b[31mcurrent\x1b[0m|\x1b[32mexpected\x1b[0m)"
+    );
+}
+
+#[ignore]
+#[test]
+fn test_bsd_math() {
+    init();
+    let output = run_command("fixtures/integration_tests/bsd_math.m4");
+
+    let test: TestSnapshot = read_test("fixtures/integration_tests/bsd_math.out");
+    assert_eq!(
+        output.status,
+        std::process::ExitStatus::from_raw(test.status),
+        "status (\x1b[31mcurrent\x1b[0m|\x1b[32mexpected\x1b[0m)"
+    );
+    assert_eq!(
+        String::from_utf8(output.stdout).unwrap(),
+        test.stdout,
+        "stdout (\x1b[31mcurrent\x1b[0m|\x1b[32mexpected\x1b[0m)"
+    );
+
+    assert_eq!(
+        String::from_utf8(output.stderr).unwrap(),
+        test.stderr,
+        "stderr (\x1b[31mcurrent\x1b[0m|\x1b[32mexpected\x1b[0m)"
+    );
+}
+
 #[test]
 fn test_changecom() {
     init();

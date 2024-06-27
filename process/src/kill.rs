87241e3e38610b6d30e94ac5c3d579514faf211a
Jeff Garzik <jgarzik@pobox.com>
2024-06-26 21:35:26 -0400

    kill: simplify sig list storage

--- process/src/kill.rs
+++ process/src/kill.rs
@@ -14,89 +14,94 @@ extern crate plib;
 use gettextrs::{bind_textdomain_codeset, setlocale, textdomain, LocaleCategory};
 use plib::PROJECT_NAME;
 use regex::Regex;
-use std::collections::HashMap;
 
 #[cfg(target_os = "macos")]
-pub fn get_sigmap() -> HashMap<&'static str, u32> {
-    HashMap::from([
-        ("HUP", 1),
-        ("INT", 2),
-        ("QUIT", 3),
-        ("ILL", 4),
-        ("TRAP", 5),
-        ("ABRT", 6),
-        ("EMT", 7),
-        ("FPE", 8),
-        ("KILL", 9),
-        ("BUS", 10),
-        ("SEGV", 11),
-        ("SYS", 12),
-        ("PIPE", 13),
-        ("ALRM", 14),
-        ("TERM", 15),
-        ("URG", 16),
-        ("STOP", 17),
-        ("TSTP", 18),
-        ("CONT", 19),
-        ("CHLD", 20),
-        ("TTIN", 21),
-        ("TTOU", 22),
-        ("IO", 23),
-        ("XCPU", 24),
-        ("XFSZ", 25),
-        ("VTALRM", 26),
-        ("PROF", 27),
-        ("WINCH", 28),
-        ("INFO", 29),
-        ("USR1", 30),
-        ("USR2", 31),
-    ])
-}
+const SIGLIST: [(&str, u32); 31] = [
+    ("HUP", 1),
+    ("INT", 2),
+    ("QUIT", 3),
+    ("ILL", 4),
+    ("TRAP", 5),
+    ("ABRT", 6),
+    ("EMT", 7),
+    ("FPE", 8),
+    ("KILL", 9),
+    ("BUS", 10),
+    ("SEGV", 11),
+    ("SYS", 12),
+    ("PIPE", 13),
+    ("ALRM", 14),
+    ("TERM", 15),
+    ("URG", 16),
+    ("STOP", 17),
+    ("TSTP", 18),
+    ("CONT", 19),
+    ("CHLD", 20),
+    ("TTIN", 21),
+    ("TTOU", 22),
+    ("IO", 23),
+    ("XCPU", 24),
+    ("XFSZ", 25),
+    ("VTALRM", 26),
+    ("PROF", 27),
+    ("WINCH", 28),
+    ("INFO", 29),
+    ("USR1", 30),
+    ("USR2", 31),
+];
 
 #[cfg(target_os = "linux")]
-pub fn get_sigmap() -> HashMap<&'static str, u32> {
-    HashMap::from([
-        ("HUP", 1),
-        ("INT", 2),
-        ("QUIT", 3),
-        ("ILL", 4),
-        ("TRAP", 5),
-        ("ABRT", 6),
-        ("IOT", 6),
-        ("BUS", 7),
-        ("FPE", 8),
-        ("KILL", 9),
-        ("USR1", 10),
-        ("SEGV", 11),
-        ("USR2", 12),
-        ("PIPE", 13),
-        ("ALRM", 14),
-        ("TERM", 15),
-        ("STKFLT", 16),
-        ("CHLD", 17),
-        ("CONT", 18),
-        ("STOP", 19),
-        ("TSTP", 20),
-        ("TTIN", 21),
-        ("TTOU", 22),
-        ("URG", 23),
-        ("XCPU", 24),
-        ("XFSZ", 25),
-        ("VTALRM", 26),
-        ("PROF", 27),
-        ("WINCH", 28),
-        ("IO", 29),
-        ("PWR", 30),
-        ("SYS", 31),
-    ])
+const SIGLIST: [(&str, u32); 32] = [
+    ("HUP", 1),
+    ("INT", 2),
+    ("QUIT", 3),
+    ("ILL", 4),
+    ("TRAP", 5),
+    ("ABRT", 6),
+    ("IOT", 6),
+    ("BUS", 7),
+    ("FPE", 8),
+    ("KILL", 9),
+    ("USR1", 10),
+    ("SEGV", 11),
+    ("USR2", 12),
+    ("PIPE", 13),
+    ("ALRM", 14),
+    ("TERM", 15),
+    ("STKFLT", 16),
+    ("CHLD", 17),
+    ("CONT", 18),
+    ("STOP", 19),
+    ("TSTP", 20),
+    ("TTIN", 21),
+    ("TTOU", 22),
+    ("URG", 23),
+    ("XCPU", 24),
+    ("XFSZ", 25),
+    ("VTALRM", 26),
+    ("PROF", 27),
+    ("WINCH", 28),
+    ("IO", 29),
+    ("PWR", 30),
+    ("SYS", 31),
+];
+
+fn siglist_get(name: &str) -> Option<u32> {
+    for (signame, signo) in SIGLIST.iter() {
+        if *signame == name {
+            return Some(*signo);
+        }
+    }
+
+    None
 }
 
-fn lookup_signum(sigmap: &HashMap<&str, u32>, signame: &str) -> Result<u32, &'static str> {
+fn lookup_signum(signame: &str) -> Result<u32, &'static str> {
     if signame == "0" {
         Ok(0)
     } else {
-        match sigmap.get(signame) {
-            Some(&sig_no) => Ok(sig_no),
+        match siglist_get(signame.to_uppercase().as_str()) {
+            Some(sig_no) => Ok(sig_no),
             None => Err("Unknown signal name"),
         }
     }
@@ -112,7 +117,7 @@ struct Config {
     pids: Vec<u32>,
 }
 
-fn parse_cmdline(sigmap: &HashMap<&str, u32>) -> Result<Config, &'static str> {
+fn parse_cmdline() -> Result<Config, &'static str> {
     let signame_re = Regex::new(r"^-(\w+)$").unwrap();
     let signum_re = Regex::new(r"^-(\d+)$").unwrap();
 
@@ -123,7 +128,7 @@ fn parse_cmdline(sigmap: &HashMap<&str, u32>) -> Result<Config, &'static str> {
     for arg in std::env::args().skip(1) {
         if in_args {
             if in_s_arg {
-                let sig_no = lookup_signum(sigmap, &arg)?;
+                let sig_no = lookup_signum(&arg)?;
                 mode = ConfigMode::Signal(sig_no);
                 in_s_arg = false;
             } else if arg == "-s" || arg == "--signal" {
@@ -132,13 +137,13 @@ fn parse_cmdline(sigmap: &HashMap<&str, u32>) -> Result<Config, &'static str> {
                 mode = ConfigMode::List;
             } else if arg == "--" {
                 in_args = false;
-            } else if let Some(caps) = signame_re.captures(&arg) {
-                let namestr = caps.get(1).unwrap().as_str();
-                let sig_no = lookup_signum(sigmap, namestr)?;
-                mode = ConfigMode::Signal(sig_no);
             } else if let Some(caps) = signum_re.captures(&arg) {
                 let numstr = caps.get(1).unwrap().as_str();
                 let sig_no = numstr.parse::<u32>().unwrap();
+                mode = ConfigMode::Signal(sig_no);
+            } else if let Some(caps) = signame_re.captures(&arg) {
+                let namestr = caps.get(1).unwrap().as_str();
+                let sig_no = lookup_signum(namestr)?;
                 mode = ConfigMode::Signal(sig_no);
             } else {
                 in_args = false;
@@ -162,9 +167,9 @@ fn parse_cmdline(sigmap: &HashMap<&str, u32>) -> Result<Config, &'static str> {
     Ok(Config { mode, pids })
 }
 
-fn list_signals(sigmap: &HashMap<&str, u32>) -> u32 {
+fn list_signals() -> u32 {
     let mut output = String::new();
-    for name in sigmap.keys() {
+    for (name, _) in SIGLIST.iter() {
         output.push_str(name);
         output.push(' ');
     }
@@ -194,11 +199,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     textdomain(PROJECT_NAME)?;
     bind_textdomain_codeset(PROJECT_NAME, "UTF-8")?;
 
-    let sigmap = get_sigmap();
-    let prog_cfg = parse_cmdline(&sigmap)?;
+    let prog_cfg = parse_cmdline()?;
 
     let exit_code = match prog_cfg.mode {
-        ConfigMode::List => list_signals(&sigmap),
+        ConfigMode::List => list_signals(),
         ConfigMode::Signal(sig_no) => send_signal(&prog_cfg, sig_no),
     };
 

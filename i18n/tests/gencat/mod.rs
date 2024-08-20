1e951e896e5885fb8a380246d5e6b164835e51af
rishadbaniya <rbdroid01@gmail.com>
2024-08-21 01:35:14 +0545

    test(gencat): add tests for gencat sets and messages with quotes

--- i18n/tests/gencat/mod.rs
+++ i18n/tests/gencat/mod.rs
@@ -76,3 +76,29 @@ fn gencat_sets_and_messagess() {
         Vec::new(),
     );
 }
+
+#[test]
+fn gencat_sets_and_messagess_with_quote() {
+    let cargo_manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let msg_file = cargo_manifest_dir.join("tests/gencat/sets_and_messages_with_quotes.msg");
+
+    #[cfg(not(target_os = "macos"))]
+    let expected_cat_file =
+        cargo_manifest_dir.join("tests/gencat/sets_and_messages_with_quotes_gnu_catfile.cat");
+
+    #[cfg(target_os = "macos")]
+    let expected_cat_file =
+        cargo_manifest_dir.join("tests/gencat/sets_and_messages_with_quotes_osx_catfile.cat");
+
+    let mut expected_output: Vec<u8> = Vec::new();
+    File::open(&expected_cat_file)
+        .unwrap()
+        .read_to_end(&mut expected_output)
+        .unwrap();
+
+    gencat_test(
+        &["-", msg_file.to_str().unwrap()],
+        expected_output,
+        Vec::new(),
+    );
+}

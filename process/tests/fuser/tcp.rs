01e89e21f3d8a5a4f748d4ea75b6bc75ae57238c
wandalen <wandalen.me@gmail.com>
2024-09-27 18:24:26 +0300

    [fuser] remove tokio

---
+++ process/tests/fuser/tcp.rs
@@ -0,0 +1,62 @@
+#[cfg(test)]
+mod tcp {
+    use crate::fuser::fuser_test;
+    use std::{
+        io,
+        net::{TcpListener, TcpStream},
+        process::Command,
+    };
+
+    /// Starts a TCP server on a predefined local address and port.
+    ///
+    /// **Returns:**
+    /// - An `io::Result` containing the bound `TcpListener` if successful.
+    fn start_tcp_server() -> io::Result<TcpListener> {
+        TcpListener::bind("127.0.0.1:0")
+    }
+
+    /// Waits for the TCP server to be ready by attempting to connect to it.
+    ///
+    /// **Arguments:**
+    /// - `port`: The port number where the TCP server is expected to be listening.
+    fn wait_for_tcp_server(port: u16) {
+        let address = format!("127.0.0.1:{}", port);
+
+        loop {
+            if let Ok(stream) = TcpStream::connect(&address) {
+                stream
+                    .shutdown(std::net::Shutdown::Both)
+                    .expect("Failed to close the connection");
+
+                break;
+            }
+
+            std::thread::yield_now();
+        }
+    }
+    /// Tests `fuser` with the TCP server to ensure it can find the process associated with the server.
+    ///
+    /// **Setup:**
+    /// - Starts a TCP server and waits for it to become available.
+    ///
+    /// **Assertions:**
+    /// - Verifies that the `fuser` command can find the process associated with the TCP server.
+    #[test]
+    fn test_fuser_tcp() {
+        let server = start_tcp_server().expect("Failed to start TCP server");
+        let port = server.local_addr().unwrap().port();
+        wait_for_tcp_server(port);
+
+        fuser_test(vec![format!("{}/tcp", port)], "", 0, |_, output| {
+            let manual_output = Command::new("fuser")
+                .arg(format!("{}/tcp", port))
+                .output()
+                .unwrap();
+
+            assert_eq!(output.status.code(), Some(0));
+            assert_eq!(output.stdout, manual_output.stdout);
+        });
+
+        drop(server);
+    }
+}
---
+++ process/tests/fuser/unix.rs
@@ -0,0 +1,87 @@
+mod unix {
+    use crate::fuser::fuser_test;
+    use std::{
+        fs,
+        os::unix::net::{UnixListener, UnixStream},
+        process::Command,
+        thread,
+    };
+
+    /// Starts a Unix socket server at the specified socket path.
+    ///
+    /// **Arguments:**
+    /// - `socket_path`: The path where the Unix socket will be created.
+    ///
+    /// **Returns:**
+    /// - A `Result` containing the bound `UnixListener` if successful, or an `io::Error`.
+    fn start_unix_socket(socket_path: &str) -> Result<UnixListener, std::io::Error> {
+        if fs::metadata(socket_path).is_ok() {
+            fs::remove_file(socket_path).map_err(|e| {
+                eprintln!("Failed to delete existing socket: {}", e);
+                e
+            })?;
+        }
+        UnixListener::bind(socket_path).map_err(|e| {
+            eprintln!("Failed to bind Unix socket: {}", e);
+            e
+        })
+    }
+
+    /// Waits for the Unix socket server to be ready by attempting to connect to it.
+    ///
+    /// **Arguments:**
+    /// - `socket_path`: The path of the Unix socket to connect to.
+    fn wait_for_unix_socket(socket_path: &str) {
+        loop {
+            match UnixStream::connect(socket_path) {
+                Ok(stream) => {
+                    if let Err(e) = stream.shutdown(std::net::Shutdown::Both) {
+                        eprintln!("Failed to close the connection: {}", e);
+                    }
+                    break;
+                }
+                Err(_) => {
+                    thread::sleep(std::time::Duration::from_millis(10));
+                }
+            }
+        }
+    }
+
+    /// Tests `fuser` with a Unix socket to ensure it can find the process associated with the socket.
+    ///
+    /// **Setup:**
+    /// - Starts a Unix socket server.
+    ///
+    /// **Assertions:**
+    /// - Verifies that the `fuser` command can find the process associated with the Unix socket.
+    #[test]
+    fn test_fuser_unixsocket() {
+        let socket_path = "/tmp/test.sock";
+        let _unix_socket = match start_unix_socket(socket_path) {
+            Ok(socket) => socket,
+            Err(e) => {
+                eprintln!("Failed to start Unix socket: {}", e);
+                return;
+            }
+        };
+
+        wait_for_unix_socket(socket_path);
+
+        let handle = thread::spawn(move || {
+            fuser_test(vec![socket_path.to_string()], "", 0, |_, _output| {
+                let manual_output = Command::new("fuser").arg(socket_path).output();
+
+                match manual_output {
+                    Ok(output) => {
+                        assert_eq!(output.status.code(), Some(0));
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to run fuser command: {}", e);
+                    }
+                }
+            });
+        });
+
+        handle.join().expect("Thread panicked");
+    }
+}

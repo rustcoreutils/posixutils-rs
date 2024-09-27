6c62e25a25a60ad19b75fce88e50876a3ed6ecc5
fox0 <15684995+fox0@users.noreply.github.com>
2024-09-27 18:59:18 +0700

    ftw/src/dir.rs revert changes

--- ftw/src/dir.rs
+++ ftw/src/dir.rs
@@ -87,7 +87,7 @@ impl OwnedDir {
         Ok(dir)
     }
 
-    pub fn iter(&self) -> OwnedDirIterator<'_> {
+    pub fn iter<'a>(&'a self) -> OwnedDirIterator<'a> {
         OwnedDirIterator {
             dirp: self.dirp,
             phantom: PhantomData,
@@ -150,7 +150,7 @@ impl DeferredDir {
         }
     }
 
-    pub fn iter(&self) -> DeferredDirIterator<'_> {
+    pub fn iter<'a>(&'a self) -> DeferredDirIterator<'a> {
         let file_descriptor = self.open_file_descriptor();
         let dir = OwnedDir::new(file_descriptor).unwrap();
         let dirp = dir.dirp;

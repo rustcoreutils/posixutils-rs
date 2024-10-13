9b6f2e823a600dbf21e627ea124f4a1ed0ae583a
rishadbaniya <rbdroid01@gmail.com>
2024-10-14 02:43:38 +0545

    test(iconv): add test cases for utf32le to ascii, utf8, utf16 LE,BE and utf32BE

--- i18n/tests/iconv/mod.rs
+++ i18n/tests/iconv/mod.rs
@@ -382,3 +382,178 @@ fn iconv_ASCII_to_UTF32BE_conversion_without_c_flag() {
         Vec::new(),
     );
 }
+
+#[test]
+fn iconv_UTF32LE_to_UTF32BE_conversion_without_c_flag() {
+    let cargo_manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+
+    let input_file = cargo_manifest_dir.join("tests/iconv/test_data_utf32le");
+    let mut input: Vec<u8> = Vec::new();
+    File::open(&input_file)
+        .unwrap()
+        .read_to_end(&mut input)
+        .unwrap();
+
+    let expected_output_file =
+        cargo_manifest_dir.join("tests/iconv/test_data_utf32le_to_utf32be_without_c_flag");
+
+    let mut expected_output: Vec<u8> = Vec::new();
+    File::open(&expected_output_file)
+        .unwrap()
+        .read_to_end(&mut expected_output)
+        .unwrap();
+
+    iconv_test(
+        &["-f", "UTF-32LE", "-t", "UTF-32BE"],
+        input.clone(),
+        expected_output.clone(),
+        Vec::new(),
+    );
+
+    iconv_test(
+        &["-f", "UTF-32LE", "-t", "UTF-32BE", "-"],
+        input,
+        expected_output,
+        Vec::new(),
+    );
+}
+
+#[test]
+fn iconv_UTF32LE_to_ASCII_conversion_with_c_flag() {
+    let cargo_manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+
+    let input_file = cargo_manifest_dir.join("tests/iconv/test_data_utf32le");
+    let mut input: Vec<u8> = Vec::new();
+    File::open(&input_file)
+        .unwrap()
+        .read_to_end(&mut input)
+        .unwrap();
+
+    let expected_output_file =
+        cargo_manifest_dir.join("tests/iconv/test_data_utf32le_to_ascii_with_c_flag");
+
+    let mut expected_output: Vec<u8> = Vec::new();
+    File::open(&expected_output_file)
+        .unwrap()
+        .read_to_end(&mut expected_output)
+        .unwrap();
+
+    iconv_test(
+        &["-c", "-f", "UTF-32LE", "-t", "ASCII"],
+        input.clone(),
+        expected_output.clone(),
+        Vec::new(),
+    );
+
+    iconv_test(
+        &["-c", "-f", "UTF-32LE", "-t", "ASCII", "-"],
+        input,
+        expected_output,
+        Vec::new(),
+    );
+}
+
+#[test]
+fn iconv_UTF32LE_to_UTF8_conversion_without_c_flag() {
+    let cargo_manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+
+    let input_file = cargo_manifest_dir.join("tests/iconv/test_data_utf32le");
+    let mut input: Vec<u8> = Vec::new();
+    File::open(&input_file)
+        .unwrap()
+        .read_to_end(&mut input)
+        .unwrap();
+
+    let expected_output_file =
+        cargo_manifest_dir.join("tests/iconv/test_data_utf32le_to_utf8_without_c_flag");
+
+    let mut expected_output: Vec<u8> = Vec::new();
+    File::open(&expected_output_file)
+        .unwrap()
+        .read_to_end(&mut expected_output)
+        .unwrap();
+
+    iconv_test(
+        &["-f", "UTF-32LE", "-t", "UTF-8"],
+        input.clone(),
+        expected_output.clone(),
+        Vec::new(),
+    );
+
+    iconv_test(
+        &["-f", "UTF-32LE", "-t", "UTF-8", "-"],
+        input,
+        expected_output,
+        Vec::new(),
+    );
+}
+
+#[test]
+fn iconv_UTF32LE_to_UTF16LE_conversion_without_c_flag() {
+    let cargo_manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+
+    let input_file = cargo_manifest_dir.join("tests/iconv/test_data_utf32le");
+    let mut input: Vec<u8> = Vec::new();
+    File::open(&input_file)
+        .unwrap()
+        .read_to_end(&mut input)
+        .unwrap();
+
+    let expected_output_file =
+        cargo_manifest_dir.join("tests/iconv/test_data_utf32le_to_utf16le_without_c_flag");
+
+    let mut expected_output: Vec<u8> = Vec::new();
+    File::open(&expected_output_file)
+        .unwrap()
+        .read_to_end(&mut expected_output)
+        .unwrap();
+
+    iconv_test(
+        &["-f", "UTF-32LE", "-t", "UTF-16LE"],
+        input.clone(),
+        expected_output.clone(),
+        Vec::new(),
+    );
+
+    iconv_test(
+        &["-f", "UTF-32LE", "-t", "UTF-16LE", "-"],
+        input,
+        expected_output,
+        Vec::new(),
+    );
+}
+
+#[test]
+fn iconv_UTF32LE_to_UTF16BE_conversion_without_c_flag() {
+    let cargo_manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+
+    let input_file = cargo_manifest_dir.join("tests/iconv/test_data_utf32le");
+    let mut input: Vec<u8> = Vec::new();
+    File::open(&input_file)
+        .unwrap()
+        .read_to_end(&mut input)
+        .unwrap();
+
+    let expected_output_file =
+        cargo_manifest_dir.join("tests/iconv/test_data_utf32le_to_utf16be_without_c_flag");
+
+    let mut expected_output: Vec<u8> = Vec::new();
+    File::open(&expected_output_file)
+        .unwrap()
+        .read_to_end(&mut expected_output)
+        .unwrap();
+
+    iconv_test(
+        &["-f", "UTF-32LE", "-t", "UTF-16BE"],
+        input.clone(),
+        expected_output.clone(),
+        Vec::new(),
+    );
+
+    iconv_test(
+        &["-f", "UTF-32LE", "-t", "UTF-16BE", "-"],
+        input,
+        expected_output,
+        Vec::new(),
+    );
+}

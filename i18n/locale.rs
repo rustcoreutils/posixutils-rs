ecf5eba7bb72ee30b3a3877f74456fcc70151a56
rishadbaniya <rbdroid01@gmail.com>
2024-10-17 23:02:28 +0545

    feat(locale): add support for printing env variables

--- i18n/locale.rs
+++ i18n/locale.rs
@@ -1,8 +1,9 @@
-use std::process::exit;
+use std::{env, process::exit};
 
 use clap::Parser;
-use gettextrs::gettext;
+use gettextrs::{bind_textdomain_codeset, gettext, setlocale, textdomain, LocaleCategory};
 use locale_lib::{read_available_charmap_names, read_available_locale_names};
+use plib::PROJECT_NAME;
 
 mod locale_lib;
 
@@ -26,25 +27,54 @@ struct Args {
 }
 
 fn display_all_locale_names() -> Result<(), Box<dyn std::error::Error>> {
-    let locale_names = read_available_locale_names()?;
-    for locale_name in locale_names {
-        println!("{locale_name}");
-    }
+    read_available_locale_names()?
+        .iter()
+        .for_each(|name| println!("{name}"));
     Ok(())
 }
 
 fn display_all_charmap_names() -> Result<(), Box<dyn std::error::Error>> {
-    let charmap_names = read_available_charmap_names()?;
-    for charmap_name in charmap_names {
-        if charmap_name.len() > 0 {
-            println!("{charmap_name}");
+    read_available_charmap_names()?
+        .into_iter()
+        .filter(|name| !name.is_empty()) // TODO: remove this filter when the bug is fixed
+        .for_each(|name| println!("{name}"));
+    Ok(())
+}
+
+fn display_env_variables() {
+    let lc_all = env::var("LC_ALL").unwrap_or_default();
+    let lang = env::var("LANG").unwrap_or_default();
+    let lc_variables = [
+        "LC_CTYPE",
+        "LC_COLLATE",
+        "LC_MONETARY",
+        "LC_NUMERIC",
+        "LC_TIME",
+        "LC_MESSAGES",
+    ];
+
+    println!("LANG={}", lang);
+
+    for &var in &lc_variables {
+        let value = std::env::var(var).unwrap_or_default();
+        if !lc_all.is_empty() {
+            println!("{}=\"{}\"", var, lc_all);
+        } else if !value.is_empty() {
+            println!("{}={}", var, value);
+        } else {
+            println!("{}=\"{}\"", var, lang);
         }
     }
-    Ok(())
+
+    println!("LC_ALL={}", lc_all);
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
+
+    setlocale(LocaleCategory::LcAll, "");
+    textdomain(PROJECT_NAME)?;
+    bind_textdomain_codeset(PROJECT_NAME, "UTF-8")?;
 
     if args.all_locales {
         display_all_locale_names()?;
@@ -55,6 +85,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         display_all_charmap_names()?;
         exit(0)
     }
+
+    display_env_variables();
 
     Ok(())
 }

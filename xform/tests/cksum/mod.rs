029be799f2f6fb96354a371fbf82df9810ae1670
fox0 <15684995+fox0@users.noreply.github.com>
2024-10-20 11:41:54 +0700

    xform: Use compile-time macro env!

--- xform/tests/cksum/mod.rs
+++ xform/tests/cksum/mod.rs
@@ -7,7 +7,7 @@
 // SPDX-License-Identifier: MIT
 //
 
-use plib::{run_test, TestPlan};
+use plib::testing::{run_test, TestPlan};
 
 fn cksum_test(test_data: &str, expected_output: &str) {
     run_test(TestPlan {

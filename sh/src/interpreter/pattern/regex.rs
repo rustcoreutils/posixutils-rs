52c3fee8df8285fd0502e8eb035c1e6ea8735393
grisenti <emanuele@grisenti.net>
2025-01-14 22:45:25 +0100

    sh: replace regex crate with libc regex

--- sh/src/interpreter/pattern/regex.rs
+++ sh/src/interpreter/pattern/regex.rs
@@ -1,20 +1,135 @@
+use core::fmt;
+use std::ffi::CString;
 use crate::interpreter::pattern::parse::{
     BracketExpression, BracketItem, ParsedPattern, PatternItem,
 };
-use regex::Regex;
-use std::fmt::Write;
+use std::fmt::{Formatter, Write};
+use std::ptr;
 
-fn is_special_character(c: char) -> bool {
-    match c {
-        '.' | '^' | '$' | '|' | '?' | '*' | '+' | '(' | ')' | '[' | ']' | '{' | '}' | '\\' => true,
-        _ => false,
+fn regex_compilation_result(
+    status_integer: libc::c_int,
+    regex: &libc::regex_t,
+) -> Result<(), String> {
+    if status_integer != 0 {
+        let mut error_buffer = vec![b'\0'; 128];
+        unsafe {
+            libc::regerror(
+                status_integer,
+                ptr::from_ref(regex),
+                error_buffer.as_mut_ptr() as *mut libc::c_char,
+                128,
+            )
+        };
+        let error = CString::from_vec_with_nul(error_buffer)
+            .expect("error message returned from `libc::regerror` is an invalid CString");
+        Err(error
+            .into_string()
+            .expect("error message from `libc::regerror' contains invalid utf-8"))
+    } else {
+        Ok(())
     }
 }
 
-fn is_supported_character_class(class: &str) -> bool {
-    match class {
-        "alnum" | "alpha" | "ascii" | "blank" | "cntrl" | "digit" | "graph" | "lower" | "print"
-        | "punct" | "space" | "upper" | "xdigit" => true,
+
+// TODO: the implementation is copied from awk, maybe we should consider putting it in a shared crate
+pub struct Regex {
+    raw_regex: libc::regex_t,
+    regex_string: CString,
+}
+
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+pub struct RegexMatch {
+    pub start: usize,
+    pub end: usize,
+}
+
+pub struct MatchIter<'re> {
+    string: CString,
+    next_start: usize,
+    regex: &'re Regex,
+}
+
+impl Iterator for MatchIter<'_> {
+    type Item = RegexMatch;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next_start >= self.string.as_bytes().len() {
+            return None;
+        }
+        let mut match_range = libc::regmatch_t {
+            rm_so: -1,
+            rm_eo: -1,
+        };
+        let exec_status = unsafe {
+            libc::regexec(
+                ptr::from_ref(&self.regex.raw_regex),
+                self.string.as_ptr().add(self.next_start),
+                1,
+                ptr::from_mut(&mut match_range),
+                0,
+            )
+        };
+        if exec_status == libc::REG_NOMATCH {
+            return None;
+        }
+        let result = RegexMatch {
+            start: self.next_start + match_range.rm_so as usize,
+            end: self.next_start + match_range.rm_eo as usize,
+        };
+        self.next_start += match_range.rm_eo as usize;
+        Some(result)
+    }
+}
+
+impl Regex {
+    pub fn new(regex: CString) -> Result<Self, String> {
+        let mut raw = unsafe { std::mem::zeroed::<libc::regex_t>() };
+        // difference from awk implementation: use 0 instead of REG_EXTENDED
+        let compilation_status =
+            unsafe { libc::regcomp(ptr::from_mut(&mut raw), regex.as_ptr(), 0) };
+        regex_compilation_result(compilation_status, &raw)?;
+        Ok(Self {
+            raw_regex: raw,
+            regex_string: regex,
+        })
+    }
+
+    pub fn match_locations(&self, string: CString) -> MatchIter {
+        MatchIter {
+            next_start: 0,
+            regex: self,
+            string,
+        }
+    }
+
+    pub fn matches(&self, string: &CString) -> bool {
+        self.match_locations(string.clone()).next().is_some()
+    }
+}
+
+impl Drop for Regex {
+    fn drop(&mut self) {
+        unsafe {
+            libc::regfree(ptr::from_mut(&mut self.raw_regex));
+        }
+    }
+}
+
+impl fmt::Display for Regex {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.regex_string.to_str().unwrap())
+    }
+}
+
+impl PartialEq for Regex {
+    fn eq(&self, other: &Self) -> bool {
+        self.regex_string == other.regex_string
+    }
+}
+
+fn is_special_character(c: char) -> bool {
+    match c {
+        // BRE special characters
+        '.' | '[' | '\\' | '*' | '^' | '$' => true,
         _ => false,
     }
 }
@@ -22,7 +137,7 @@ fn is_supported_character_class(class: &str) -> bool {
 fn push_char_literal(c: char, string: &mut String) {
     match c {
         c if is_special_character(c) => {
-            write!(string, "\\x{{{:X}}}", c as u32).unwrap();
+            write!(string, "\\{c}").unwrap();
         }
         _ => string.push(c),
     }
@@ -38,9 +153,6 @@ fn push_bracket_expression(expr: &BracketExpression, string: &mut String) {
             BracketItem::Char(c) => push_char_literal(*c, string),
             BracketItem::CharacterClass(class) => {
                 string.push_str("[:");
-                if !is_supported_character_class(class) {
-                    todo!("unsupported character class: {}", class);
-                }
                 string.push_str(class);
                 string.push_str(":]");
             }
@@ -50,7 +162,7 @@ fn push_bracket_expression(expr: &BracketExpression, string: &mut String) {
     string.push(']');
 }
 
-pub fn parsed_pattern_to_regex(parsed_pattern: &ParsedPattern) -> Result<Regex, regex::Error> {
+pub fn parsed_pattern_to_regex(parsed_pattern: &ParsedPattern) -> Result<Regex, String> {
     let mut regex_str = String::new();
     for item in &parsed_pattern.items {
         match item {
@@ -60,7 +172,8 @@ pub fn parsed_pattern_to_regex(parsed_pattern: &ParsedPattern) -> Result<Regex, 
             PatternItem::BracketExpression(expr) => push_bracket_expression(expr, &mut regex_str),
         }
     }
-    Regex::new(&regex_str)
+    let regex_cstr = CString::new(regex_str).expect("pattern cannot contain null characters");
+    Regex::new(regex_cstr)
 }
 
 #[cfg(test)]
@@ -228,25 +341,17 @@ mod tests {
     }
 
     #[test]
-    fn special_regex_characters_are_escaped() {
+    fn special_bre_characters_are_escaped() {
         let regex = pattern_to_regex_string(ParsedPattern {
             items: vec![
                 PatternItem::Char('.'),
+                PatternItem::Char('['),
+                PatternItem::Char('\\'),
+                PatternItem::Char('*'),
                 PatternItem::Char('^'),
                 PatternItem::Char('$'),
-                PatternItem::Char('|'),
-                PatternItem::Char('?'),
-                PatternItem::Char('*'),
-                PatternItem::Char('+'),
-                PatternItem::Char('('),
-                PatternItem::Char(')'),
-                PatternItem::Char('['),
-                PatternItem::Char(']'),
-                PatternItem::Char('{'),
-                PatternItem::Char('}'),
-                PatternItem::Char('\\'),
             ],
         });
-        assert_eq!(regex, "\\x{2E}\\x{5E}\\x{24}\\x{7C}\\x{3F}\\x{2A}\\x{2B}\\x{28}\\x{29}\\x{5B}\\x{5D}\\x{7B}\\x{7D}\\x{5C}");
+        assert_eq!(regex, "\\.\\[\\\\\\*\\^\\$");
     }
 }

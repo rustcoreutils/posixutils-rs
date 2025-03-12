6ca2ef0e4254587892dbead183ba5da489fbd1d8
grisenti <emanuele@grisenti.net>
2025-03-12 15:16:46 +0100

    sh: improve test

--- sh/tests/sh/builtin/dot.sh
+++ sh/tests/sh/builtin/dot.sh
@@ -1,8 +1,26 @@
-cd $TEST_READ_DIR
+echo 'echo hello' > $TEST_WRITE_DIR/dot.txt
+. $TEST_WRITE_DIR/dot.txt
+echo $?
 
-. ./file2.txt
+echo 'echo $LINENO
+echo $LINENO' > $TEST_WRITE_DIR/dot.txt
+. $TEST_WRITE_DIR/dot.txt
 echo $?
 
 x=value y=other_value
-. ./filea.txt
+echo 'echo $x $y' > $TEST_WRITE_DIR/dot.txt
+. $TEST_WRITE_DIR/dot.txt
 echo $?
+
+echo 'echo hello' > $TEST_WRITE_DIR/dot.txt
+. $TEST_WRITE_DIR/dot.txt > $TEST_WRITE_DIR/dot_out.txt
+echo $?
+cat $TEST_WRITE_DIR/dot_out.txt
+rm $TEST_WRITE_DIR/dot_out.txt
+
+echo 'false' > $TEST_WRITE_DIR/dot.txt
+. $TEST_WRITE_DIR/dot.txt
+echo $?
+
+rm $TEST_WRITE_DIR/dot.txt
+

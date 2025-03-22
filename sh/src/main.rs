494db4933eff7bbb67b2c43afc41a1f5ee35d6ef
grisenti <emanuele@grisenti.net>
2025-03-22 10:09:47 +0100

    sh: setup job control for interactive shells

--- sh/src/main.rs
+++ sh/src/main.rs
@@ -7,14 +7,15 @@
 // SPDX-License-Identifier: MIT
 //
 
-use crate::builtin::trap::TrapAction;
 use crate::cli::{parse_args, ExecutionMode};
 use crate::shell::Shell;
-use crate::signals::{setup_signal_handling, Signal};
+use crate::signals::setup_signal_handling;
 use atty::Stream;
 use nix::libc;
+use nix::sys::signal::{sigaction, SaFlags, SigAction, SigSet};
+use nix::sys::signal::{SigHandler, Signal as NixSignal};
 use std::io;
-use std::os::fd::RawFd;
+use std::os::fd::AsFd;
 use std::time::Duration;
 
 mod builtin;
@@ -54,6 +55,61 @@ fn execute_string(string: &str, shell: &mut Shell) {
     }
 }
 
+fn interactive_shell() {
+    let pgid = nix::unistd::getpgrp();
+    nix::unistd::tcsetpgrp(io::stdin().as_fd(), pgid).unwrap();
+    nix::fcntl::fcntl(
+        libc::STDIN_FILENO,
+        nix::fcntl::FcntlArg::F_SETFL(nix::fcntl::OFlag::O_NONBLOCK),
+    )
+    .unwrap();
+    let ignore_action = SigAction::new(SigHandler::SigIgn, SaFlags::empty(), SigSet::empty());
+    unsafe {
+        sigaction(NixSignal::SIGQUIT, &ignore_action).unwrap();
+    }
+    unsafe {
+        sigaction(NixSignal::SIGTERM, &ignore_action).unwrap();
+    }
+    if get_global_shell().set_options.monitor {
+        // job control signals
+        unsafe {
+            sigaction(NixSignal::SIGTTIN, &ignore_action).unwrap();
+        }
+        unsafe {
+            sigaction(NixSignal::SIGTTOU, &ignore_action).unwrap();
+        }
+        unsafe {
+            sigaction(NixSignal::SIGTSTP, &ignore_action).unwrap();
+        }
+    }
+    let mut buffer = String::new();
+    eprint!("{}", get_global_shell().get_ps1());
+    loop {
+        while io::stdin().read_line(&mut buffer).is_ok_and(|n| n > 0) {
+            if buffer.ends_with("\\\n") {
+                continue;
+            }
+            match get_global_shell().execute_program(&buffer) {
+                Ok(_) => {
+                    buffer.clear();
+                    eprint!("{}", get_global_shell().get_ps1());
+                }
+                Err(syntax_err) => {
+                    if !syntax_err.could_be_resolved_with_more_input {
+                        eprintln!("sh: syntax error: {}", syntax_err.message);
+                        buffer.clear();
+                        eprint!("{}", get_global_shell().get_ps1());
+                    } else {
+                        eprint!("{}", get_global_shell().get_ps2());
+                    }
+                }
+            }
+        }
+        std::thread::sleep(Duration::from_millis(16));
+        get_global_shell().process_signals();
+    }
+}
+
 fn main() {
     let is_attached_to_terminal = atty::is(Stream::Stdin) && atty::is(Stream::Stdout);
     let args = parse_args(std::env::args().collect(), is_attached_to_terminal).unwrap();
@@ -68,39 +124,7 @@ fn main() {
     unsafe { libc::atexit(on_exit) };
     unsafe { setup_signal_handling() };
     match args.execution_mode {
-        ExecutionMode::Interactive => {
-            nix::fcntl::fcntl(
-                libc::STDIN_FILENO,
-                nix::fcntl::FcntlArg::F_SETFL(nix::fcntl::OFlag::O_NONBLOCK),
-            )
-            .unwrap();
-            let mut buffer = String::new();
-            eprint!("{}", get_global_shell().get_ps1());
-            loop {
-                while io::stdin().read_line(&mut buffer).is_ok_and(|n| n > 0) {
-                    if buffer.ends_with("\\\n") {
-                        continue;
-                    }
-                    match get_global_shell().execute_program(&buffer) {
-                        Ok(_) => {
-                            buffer.clear();
-                            eprint!("{}", get_global_shell().get_ps1());
-                        }
-                        Err(syntax_err) => {
-                            if !syntax_err.could_be_resolved_with_more_input {
-                                eprintln!("sh: syntax error: {}", syntax_err.message);
-                                buffer.clear();
-                                eprint!("{}", get_global_shell().get_ps1());
-                            } else {
-                                eprint!("{}", get_global_shell().get_ps2());
-                            }
-                        }
-                    }
-                }
-                std::thread::sleep(Duration::from_millis(16));
-                get_global_shell().process_signals();
-            }
-        }
+        ExecutionMode::Interactive => interactive_shell(),
         ExecutionMode::ReadCommandsFromStdin => {
             let mut buffer = String::new();
             while io::stdin().read_line(&mut buffer).is_ok_and(|n| n > 0) {

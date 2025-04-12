29614a67bf04529fd009533abe7a688cbe9a46ad
grisenti <emanuele@grisenti.net>
2025-04-12 23:44:38 +0200

    sh: add license

--- sh/os/mod.rs
+++ sh/os/mod.rs
@@ -1,3 +1,12 @@
+//
+// Copyright (c) 2024 Hemi Labs, Inc.
+//
+// This file is part of the posixutils-rs project covered under
+// the MIT License.  For the full license text, please see the LICENSE
+// file in the root directory of this project.
+// SPDX-License-Identifier: MIT
+//
+
 use crate::os::errno::{get_current_errno_value, Errno};
 use crate::os::signals::Signal;
 use crate::shell::environment::Environment;

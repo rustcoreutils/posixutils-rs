1bc304e4466ce3bb6bd9f9d41402aa0901852005
grisenti <emanuele@grisenti.dev>
2026-08-01 22:04:44 +0200

    sh: improve tests

--- sh/tests/sh/word_expansion/field_splitting_can_create_empty_fields.sh
+++ sh/tests/sh/word_expansion/field_splitting_can_create_empty_fields.sh
@@ -1,5 +1,17 @@
 IFS=:
-VAR='a::b:::c'
+VAR='a::b:::c::'
+for x in $VAR; do
+  echo $x
+done
+
+IFS=': '
+VAR='   a:  b: :c:   '
+for x in $VAR; do
+  echo $x
+done
+
+IFS=' '
+VAR=' '
 for x in $VAR; do
   echo $x
 done
